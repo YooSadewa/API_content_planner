605824dc673bf9f9368493a37112975852b96d3d
YooSadewa <tugasthyy@gmail.com>
2025-01-20 10:11:38 +0700

    Create speaker module

--- app/Http/Controllers/api/ApiController.php
+++ app/Http/Controllers/api/ApiController.php
@@ -5,20 +5,39 @@ namespace App\Http\Controllers\api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Host;
+use App\Models\Pembicara;
 use Illuminate\Support\Facades\Validator;
 
 class ApiController extends Controller
 {
     //Module Host
     public function getHost() {
-        $host = Host::all();
-        return response()->json([
-            'success' => true,
-            'message' => 'Data Host berhasil diambil',
-            'data' => [
-                'host' => $host
-            ]
-        ], 200);
+        try {
+            $host = Host::all();
+        
+            if ($host->isEmpty()) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Data Host tidak ditemukan',
+                    'data' => null
+                ], 404);
+            }
+        
+            return response()->json([
+                'status' => true,
+                'message' => 'Data Host berhasil diambil',
+                'data' => [
+                    'host' => $host
+                ]
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Terjadi kesalahan saat mengambil data host',
+                'error' => $e->getMessage()
+            ], 500);
+        }
+        
     }
 
     public function createHost(Request $request) {
@@ -89,10 +108,11 @@ class ApiController extends Controller
                         ],
                     ], 200);
                 }
-        } catch (\Exception) {
+        } catch (\Exception $e) {
             return response()->json([
                 'status' => false,
                 'message' => 'Host gagal diupdate',
+                'data' => $e->getMessage(),
             ], 500); 
         }
     }
@@ -123,5 +143,138 @@ class ApiController extends Controller
     }
     //End module
 
-    //Module pembicara
+    //Module Pembicara
+    public function getSpeaker() {
+        try {
+            $speaker = Pembicara::all();
+
+            if($speaker->isEmpty()) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Pembicara tidak ditemukan',
+                    'data' => null
+                ], 404);
+            } else {
+                return response()->json([
+                    'status' => true,
+                    'message' => 'Pembicara berhasil ditemukan',
+                    'data' => [
+                        'pembicara' => $speaker
+                    ]
+                ], 200);
+            }
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Terjadi kesalahan saat mengambil data pembicara',
+                'error' => $e->getMessage()
+            ], 500);
+        }
+    }
+
+    public function createSpeaker(Request $request) {
+        $validator = Validator::make($request->all(), [
+            'pmb_nama' => 'required|string|max:255|unique:pembicaras'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Validator Error',
+                'data' => $validator->errors()
+            ], 422);
+        }
+
+        try {
+            $speaker = Pembicara::create([
+                'pmb_nama' => $request->pmb_nama,
+            ]);
+
+            return response()->json([
+                'status' => true,
+                'message' => 'Pembicara berhasil dibuat',
+                'data' => [
+                    'pembicara' => $speaker
+                ]
+            ], 201);
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Terjadi kesalahan saat membuat pembicara',
+                'error' => $e->getMessage()
+            ], 500);
+        }
+    }
+
+    public function updateSpeaker($id, Request $request) {
+        $speaker = Pembicara::where('pmb_id', $id)->first();
+
+        if(!$speaker) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Pembicara tidak ditemukan',
+            ], 404);
+        }
+
+        $validator = Validator::make($request->all(), [
+            'pmb_nama' => 'required|string|max:255|unique:pembicaras,pmb_nama,' . $id . ',pmb_id'
+        ]);
+
+        if($validator->fails()) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Validator Error',
+                'data' => $validator->errors()
+            ], 422);
+        }
+
+        try {
+            $updateSpeaker = $speaker->update([
+                'pmb_nama' => $request->pmb_nama,
+            ]);
+
+            if ($updateSpeaker) {
+                return response()->json([
+                    'status' => true,
+                    'message' => 'Pembicara berhasil diupdate',
+                    'data' => [
+                        'pembicara' => $speaker
+                    ]
+                ], 200);
+            }
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Terjadi kesalahan saat mengupdate pembicara',
+                'error' => $e->getMessage()
+            ], 500);
+        }
+    }
+
+    public function deleteSpeaker($id) {
+        try {
+            $speaker = Pembicara::find($id);
+
+            if(!$speaker) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Pembicara tidak ditemukan',
+                ], 404);
+            }
+
+            $speaker->delete();
+
+            return response()->json([
+                'status' => true,
+                'message' => 'Pembicara berhasil dihapus',
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Terjadi kesalahan saat menghapus pembicara',
+                'error' => $e->getMessage()
+            ], 500);
+        }
+    }
+    //End module
 }
--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,14 @@ use App\Http\Controllers\api\ApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+//Host route
 Route::get('/host', [ApiController::class, 'getHost']);
 Route::post('/host/create', [ApiController::class, 'createHost']);
 Route::put('/host/update/{id}', [ApiController::class, 'updateHost']);
 Route::delete('/host/delete/{id}', [ApiController::class, 'deleteHost']);
+
+//Speaker route
+Route::get('/pembicara', [ApiController::class, 'getSpeaker']);
+Route::post('/pembicara/create', [ApiController::class, 'createSpeaker']);
+Route::put('/pembicara/update/{id}', [ApiController::class, 'updateSpeaker']);
+Route::delete('/pembicara/delete/{id}', [ApiController::class, 'deleteSpeaker']);
